b5ac51cbd27546e9128ab7774761d70b08426df0
ImaevDmitrii <121541029+ImaevDmitrii@users.noreply.github.com>
2023-01-09 13:17:04 +0300

    Building Lists and Navigation

---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Дмитрий Имаев on 09.01.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
