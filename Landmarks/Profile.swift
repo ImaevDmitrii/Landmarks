3fa275ac288744e6b92315cee401475426d21fe0
ImaevDmitrii <121541029+ImaevDmitrii@users.noreply.github.com>
2023-01-10 16:04:16 +0300

    Working with UI Controls

---
+++ Landmarks/Profile.swift
@@ -0,0 +1,26 @@
+//
+//  Profile.swift
+//  Landmarks
+//
+//  Created by Дмитрий Имаев on 10.01.2023.
+//
+
+import Foundation
+
+struct Profile {
+    var username: String
+    var prefersNotifications = true
+    var seasonalPhoto = Season.winter
+    var goalDate = Date()
+    
+    static let `default` = Profile(username: "g_kumar")
+    
+    enum Season: String, CaseIterable, Identifiable {
+        case spring = "🌷"
+        case summer = "🌞"
+        case autumn = "🍂"
+        case winter = "☃️"
+        
+        var id: String {rawValue}
+    }
+}
